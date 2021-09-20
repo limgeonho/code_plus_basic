bc7bce7238d7c3e6091d9016f881cc73b7de51ba
limgeonho <ghlim909@gmail.com>
2021-09-20 23:04:50 +0900

    2021.09.20

--- 다이나믹프로그래밍_1/knapsack/12865_평범한배낭1.py
+++ 다이나믹프로그래밍_1/knapsack/12865_평범한배낭1.py
@@ -1,1 +1,12 @@
-# 평범한배낭1
+# 평범한 배낭1
+
+n, limit = map(int, input().split())
+
+dp = [0] * (limit+1)
+
+for i in range(n):
+    weight, value = map(int, input().split())
+    for j in range(limit, weight-1, -1):
+        dp[j] = max(dp[j], dp[j-weight] + value)
+
+print(dp[limit])
--- 다이나믹프로그래밍_1/knapsack/3067_coins.py
+++ 다이나믹프로그래밍_1/knapsack/3067_coins.py
@@ -7,5 +7,11 @@ for _ in range(T):
     coins = list(map(int, input().split()))
     m = int(input())
 
-    cnt = 0
-    
+    dp = [0 for _ in range(m+1)]
+    dp[0] = 1
+
+    for i in range(n):
+        for j in range(coins[i], m+1):
+            dp[j] += dp[j-coins[i]]
+    print(dp[m])
+
