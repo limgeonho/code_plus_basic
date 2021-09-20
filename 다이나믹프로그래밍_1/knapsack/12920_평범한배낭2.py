c9efca08ae2a49484ea2525f4abb2ee6fbf71f05
limgeonho <ghlim909@gmail.com>
2021-09-20 23:49:23 +0900

    2021.09.20

--- 다이나믹프로그래밍_1/knapsack/12920_평범한배낭2.py
+++ 다이나믹프로그래밍_1/knapsack/12920_평범한배낭2.py
@@ -1,8 +1,8 @@
 # 평범한 배낭2
 
-n, m = map(int, input().split())
-
-for _ in range(n):
-    v, c, k = map(int, input().split())
-    for i in range(m+1):
-        if 
+# n, m = map(int, input().split())
+#
+# for _ in range(n):
+#     v, c, k = map(int, input().split())
+#     for i in range(m+1):
+#         if
